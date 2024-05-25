3f3a342507b069655b64965c7e968aef4e311fa3
Dorian Izaiah Brown <devorian143@gmail.com>
2024-05-25 13:53:20 -0700

    Jump with space bar and clean up code
    Instead of using vertical input, we use space to jump. This fixed the issue of us jumping when you press d or down. I also cleaned up the code and added comments for clarity.

--- Natssal/Assets/Player Character/PlayerMovement.cs
+++ Natssal/Assets/Player Character/PlayerMovement.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-
-    public float groundSpeed;
-    public float jumpSpeed;
-    public Rigidbody2D body;
     [Range(0f, 1f)] // Adds a slider to control this property in Unity Inspector
     public float groundDecay;
-    public bool isGrounded;
+    public float acceleration; 
+    public float maxSpeed;
+
+    public float jumpSpeed;
+
     public BoxCollider2D groundCheck;
     public LayerMask groundMask;
+    public Rigidbody2D body;
+   
+    public bool isGrounded;
 
     // Does not need public since we do not need to see it in the Unity Inspector or be accessible anywhere else
     float xInput;
@@ -27,65 +30,88 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        GetInput();
-        MovePlayerWithInput();
+        CheckInput();
+        HandleJump();
     }
 
     // FixedUpdate is called with the same time between each interval
     void FixedUpdate()
     {
         CheckGround();
+        HandleHorizontalMovement();
 
-        // Hoenstly, the functionality seems no different when I comment ApplyFriction() out. So I hope I did it right lol.
+        // TODO: Honestly, the functionality seems no different when I comment ApplyFriction() out. So I hope I did it right lol.
         ApplyFriction();
     }
 
-    void GetInput()
+    void CheckInput()
     {
         // https://docs.unity3d.com/ScriptReference/Input.GetAxis.html 
         xInput = Input.GetAxis("Horizontal");
         yInput = Input.GetAxis("Vertical");
     }
 
-    void MovePlayerWithInput()
+    void HandleHorizontalMovement()
     {
+        /*
+        * Input.GetAxis returns a number different than zero if the user is giving an input.
+        * So the only axis we want to be effected is the axis the user is trying to control.
+        * The axis not being controlled should just match whatever the body's current velocity is for that axis.
+        */
         if (Mathf.Abs(xInput) > 0)
         {
+            // Increment velocity by our acceleration, then clamp within max
+            float increment = xInput * acceleration;
+            // First argument must stay in between the second and third argument
+            float newSpeed = Mathf.Clamp(body.velocity.x + increment, -maxSpeed, maxSpeed);
+
             /*
              * Vector is just a object that has magnitude and direction https://mathinsight.org/vector_introduction
              * Vector2 is a Unity object / class. That's why we create a new one and assign it to a property of the body
              */
-            body.velocity = new Vector2(xInput * groundSpeed, body.velocity.y);
+            body.velocity = new Vector2(newSpeed, body.velocity.y);
 
-            // TODO: Look more into this code and understand it. This is how we change the direction of the player.
-            float direction = Mathf.Sign(xInput);
-            transform.localScale = new Vector3(direction, 1, 1);
+            UpdateDirection();
         }
+    }
 
-        /*
-         * Input.GetAxis returns a number different than zero if the user is giving an input.
-         * So the only axis we want to be effected is the axis the user is trying to control.
-         * The axis not being controlled should just match whatever the body's current velocity is for that axis.
-         */
-        if (Mathf.Abs(yInput) > 0 && isGrounded)
+    void UpdateDirection()
+    {
+        // Determine whether the input is positive or negative
+        float direction = Mathf.Sign(xInput);
+        // Update the x value of the player scale. Negative will face left. Positive will face right.
+        transform.localScale = new Vector3(direction, 1, 1);
+    }
+
+    void HandleJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
         {
-            body.velocity = new Vector2(body.velocity.x, 1 * jumpSpeed);
+            body.velocity = new Vector2(body.velocity.x, jumpSpeed);
         }
     }
 
     void CheckGround()
     {
-        // TODO: What does this line do?
+        /*
+         * https://docs.unity3d.com/ScriptReference/Physics2D.OverlapAreaAll.html
+         * We get all colliders that fall within a rectangular area specified by the first and second argument.
+         * In this case, we are looking to see if there are any colliders within the area of our Ground Check collider.
+         * Obviously there is a collider in that area, the Ground Check collider itself. This is where the third argument comes in.
+         * We can specify a layer mask to only check for objects that have that layer mask. So that means any collider with the 
+         * specified mask that is within the area of our Ground Check collider will be returned from this function AKA they are touching.
+         * We get an array of colliders as a return value.
+         */
         isGrounded = Physics2D.OverlapAreaAll(groundCheck.bounds.min, groundCheck.bounds.max, groundMask).Length > 0;
     }
 
     void ApplyFriction()
     {
         bool isNoHorizontalInput = xInput == 0;
-        bool isNoVerticalInput = yInput == 0;
+        bool isNotJumping = body.velocity.y <= 0;
 
         // We only want to apply the ground decay when the user stop running AKA lets go of the horizontal input.
-        if (isGrounded && isNoHorizontalInput && isNoVerticalInput)
+        if (isGrounded && isNoHorizontalInput && isNotJumping)
         {
             body.velocity *= groundDecay;
         }
